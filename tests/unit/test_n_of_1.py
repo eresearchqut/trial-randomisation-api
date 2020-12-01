575c7d8cf618832303f0c160d8162810339bf239
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-12-01 14:26:37 +1000

    Shuffle schedules in blocks

--- tests/unit/test_n_of_1.py
+++ tests/unit/test_n_of_1.py
@@ -1,7 +1,7 @@
 import pytest
 
 from math import factorial, pow
-from app.n_of_1 import randomise_n_of_1_schedule, permutate, block
+from app.n_of_1 import randomise_n_of_1_schedule, permutate, block, shuffled_block
 
 
 @pytest.mark.parametrize("treatments,expected", [(2, [['A', 'B'], ['B', 'A']]),
@@ -22,9 +22,13 @@ def test_permutations(treatments, expected):
                                                                 [['B', 'A'], ['A', 'B']],
                                                                 [['B', 'A'], ['B', 'A']]])])
 def test_block_and_shuffle(cycles, treatments, expected):
-    blocks = block(cycles, treatments)
-    assert len(blocks) == pow(factorial(treatments), cycles)
-    assert blocks == expected
+    schedules = block(cycles, treatments)
+    assert len(schedules) == pow(factorial(treatments), cycles)
+    assert schedules == expected
+
+    block_generator = shuffled_block(cycles, treatments)
+    for _ in range(3):
+        assert len(next(block_generator)) == pow(factorial(treatments), cycles)
 
 
 @pytest.mark.parametrize("patients,cycles,treatments", [(4, 2, 2), (40, 3, 2)])
